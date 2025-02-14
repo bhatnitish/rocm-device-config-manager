6ce064733faa212b2054cc725dc03192b44d03c0
nikhilsk <nkomarla@amd.com>
2025-02-14 11:19:01 +0000

    amend

--- docker/build_config_manager_release_docker.sh
+++ docker/build_config_manager_release_docker.sh
@@ -59,7 +59,7 @@ IMAGE_URL="${DOCKER_REGISTRY}:${VER}"
 
 echo $TOP_DIR
 cp -r $TOP_DIR/assets/amd_smi_lib $TOP_DIR/docker/smilib
-ln -f $TOP_DIR/cmd/deviceconfigmanager/bin/amd-config-manager $TOP_DIR/docker/amd-config-manager
+ln -f $TOP_DIR/bin/amd-config-manager $TOP_DIR/docker/amd-config-manager
 
 if [ $PUBLISH_IMAGE == 1 ]; then
     echo "publishing dcm image to $IMAGE_URL"
