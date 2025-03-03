9992d0364879ac93dc19defd18e096bc40c8349b
nikhilsk <nkomarla@amd.com>
2025-03-03 09:09:34 +0000

    final

--- pkg/config_manager/configmanager.go
+++ pkg/config_manager/configmanager.go
@@ -18,7 +18,7 @@ package configmanager
 
 /*
 #cgo CFLAGS: -I/device-config-manager/assets6.4/amd_smi_lib/amd_smi
-#cgo LDFLAGS: -L/device-config-manager/assets6.4/amd_smi_lib -lamd_smi -ldrm -ldrm_amdgpu
+#cgo LDFLAGS: -L/device-config-manager/assets6.4/amd_smi_lib -lamd_smi -ldrm_amdgpu -ldrm
 #include "/device-config-manager/assets6.4/amd_smi_lib/amdsmi.h"
 */
 import "C"
