43d6c6a68da35bc07124eb32b12dbf73258f5bbd
nikhilsk <nkomarla@amd.com>
2025-04-17 11:56:26 +0000

    supporting ubuntu 24 dcm docker

--- docker/build_config_manager_release_docker.sh
+++ docker/build_config_manager_release_docker.sh
@@ -73,7 +73,11 @@ ln -f $TOP_DIR/bin/device-config-manager-$UBUNTU_VERSION $TOP_DIR/docker/device-
 
 if [ $PUBLISH_IMAGE == 1 ]; then
     echo "publishing dcm image to $IMAGE_URL"
-    docker build -t $IMAGE_URL . -f Dockerfile && docker push $IMAGE_URL
+    if [ "$UBUNTU_VERSION" = "jammy" ]; then
+        docker build -t $IMAGE_URL . -f Dockerfile && docker push $IMAGE_URL
+    elif [ "$UBUNTU_VERSION" = "noble" ]; then
+        docker build -t $IMAGE_URL . -f Dockerfile.ubuntu24  && docker push $IMAGE_URL
+    fi
     if [ $? -eq 0 ]; then
         echo "Successfully published image $IMAGE_URL"
     else
@@ -82,7 +86,11 @@ if [ $PUBLISH_IMAGE == 1 ]; then
     fi
 else
     echo "building dcm image to $DOCKER_IMAGE_NAME"
-    docker build -t $IMAGE_URL . -f Dockerfile && docker save -o config-manager-$VER.tar $IMAGE_URL
+    if [ "$UBUNTU_VERSION" = "jammy" ]; then
+        docker build -t $IMAGE_URL . -f Dockerfile && docker save -o config-manager-$VER.tar $IMAGE_URL
+    elif [ "$UBUNTU_VERSION" = "noble" ]; then
+        docker build -t $IMAGE_URL . -f Dockerfile.ubuntu24  && docker save -o config-manager-$VER.tar $IMAGE_URL
+    fi
     if [ $? -eq 0 ]; then
         gzip config-manager-$VER.tar
         mv config-manager-$VER.tar.gz config-manager-$VER.tgz
---
+++ docker/entrypoint_noble.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#
+# Copyright(C) Advanced Micro Devices, Inc. All rights reserved.
+#
+# You may not use this software and documentation (if any) (collectively,
+# the "Materials") except in compliance with the terms and conditions of
+# the Software License Agreement included with the Materials or otherwise as
+# set forth in writing and signed by you and an authorized signatory of AMD.
+# If you do not have a copy of the Software License Agreement, contact your
+# AMD representative for a copy.
+#
+# You agree that you will not reverse engineer or decompile the Materials,
+# in whole or in part, except as allowed by applicable law.
+#
+# THE MATERIALS ARE DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+# REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+#
+# entry point script run on creating a node management container
+
+unset LD_LIBRARY_PATH
+sleep 2
+# Instead of a bare exec of /home/amd/bin/server,
+# invoke the updated dynamic linker from your updated libraries.
+exec /home/amd/lib-updated/ld-linux-x86-64.so.2 \
+     --library-path /home/amd/lib-updated:/opt/rocm-6.4.0/lib \
+     /home/amd/bin/server
